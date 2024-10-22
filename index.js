82aed187d0c8afdd60e1bb9e3827fcd2b6807386
André Company Gianesi <andrecompanygianesi@Andres-MacBook-Air.local>
2024-10-22 01:00:00 -0400

    Created array of questions for user input

--- index.js
+++ index.js
@@ -1,13 +1,70 @@
 // TODO: Include packages needed for this application
-cle
+import inquirer from 'inquirer';
+import fs from 'fs';
+import generateMarkdown from './utils/generateMarkdown.js';
+
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "title",
+            message: "What is your project called?",
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Describe the purpose of the project.",
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "What are the steps required to install your project?",
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "Please elaborate on how to use it.",
+        },
+        {
+            type: "input",
+            name: "contribuiting",
+            message: "List any collaborators",
+        },
+        {
+            type: "checkbox",
+            name: "license",
+            message: "Select a license.",
+            choices: ["", "", ""],
+        },
+        {
+            type: "input",
+            name: "tests",
+            message: "What are the testing instructions?",
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "What is your GitHub username?",
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Enter your email so users can ask questions ",
+        },
+
+    ]);
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+        console.log("Generating README.md file...");
+        writeToFile("./sample_output/README.md", generateMarkdown({ ...answers }));
+    })
+}
 
 // Function call to initialize app
 init();
