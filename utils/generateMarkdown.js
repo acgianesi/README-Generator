5c8c5edea10d713c6543e555a13e18c8d21ea659
André Company Gianesi <andrecompanygianesi@Andres-MacBook-Air.local>
2024-10-22 04:12:00 -0400

    Added new function and made other changes

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -8,23 +8,38 @@ function renderLicenseBadge(license) {
   } else if (license === "Boost") {
     return "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)"
   }
-  if (license === null) {
-    return '';
+    return "";
   }
-
-}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) { }
+
+function renderLicenseLink(license) { 
+  if (license === "MIT") {
+    return "(https://opensource.org/licenses/MIT)"
+  } else if (license === "Apache") {
+    return "[(https://opensource.org/licenses/Apache-2.0)"
+  } else if (license === "Boost") {
+    return "[(https://www.boost.org/LICENSE_1_0.txt)"
+  }
+  if (license === null) {
+    return "";
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) { }
+
+function renderLicenseSection(license) {
+if(license === null)
+return "";
+return `Licensed under the ${license} license`
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+
 ${renderLicenseBadge(data.license)}
 
 ## Description
@@ -36,9 +51,9 @@ ${data.description}
 - [Installation](#installation)
 - [Usage](#usage)
 - [License](#license)
-- [Contributing] (#contributing)
-- [Tests] (#tests)
-- [Questions] (#questions)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
 
 ## Installation 
 
@@ -50,7 +65,6 @@ ${data.usage}
 
 ## License
 
-${data.license}
 ${renderLicenseSection(data.license)}
 
 ## Contributing
